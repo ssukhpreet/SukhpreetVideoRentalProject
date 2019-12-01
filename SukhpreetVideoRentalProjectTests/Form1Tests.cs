84099a1792e1f14047071e7db1ec49be3bbb8445
Husandeep Singh Hayer <HH026@atcltd.atc.org.nz>
2019-12-02 10:58:41 +1300

    Add project files.

---
+++ SukhpreetVideoRentalProjectTests/Form1Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SukhpreetVideoRentalProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SukhpreetVideoRentalProject.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        [TestMethod()]
+        public void Form1Test()
+        {
+            Form1 obj = new Form1();
+           // Assert.Fail();
+        }
+    }
+}
---
+++ SukhpreetVideoRentalProjectTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SukhpreetVideoRentalProject;
+
+namespace SukhpreetVideoRentalProjectTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            IncludeConnection obj = new IncludeConnection();
+        }
+    }
+}
